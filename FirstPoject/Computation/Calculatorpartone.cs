2ef7cc56505b4d1bb676163b7ca6ffe6427fa3f7
Nicolo Orcine <37629799+Nicolorcine@users.noreply.github.com>
2022-06-24 01:27:23 +0800

    Add project files.

---
+++ FirstPoject/Computation/Calculatorpartone.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+namespace myFirstProject.Computation
+{
+    class FirstPart
+    {
+        public static string questionAnswer;
+        public static int firstNumber;
+
+        public void operationAnswer()
+        {
+            questionAnswer = Console.ReadLine();
+            checkQuestionAnswer();
+        }
+
+
+        void firstContainer()
+        { 
+            Console.Write("Enter a number: ");
+            try { 
+                firstNumber = Int32.Parse(Console.ReadLine());
+            }
+            catch(FormatException)
+            {
+                firstContainer();
+            }
+        }
+
+
+
+        void checkQuestionAnswer()
+        {
+            if ( questionAnswer == "+" || questionAnswer == "-" || questionAnswer == "*" || questionAnswer == "/")
+            {
+                firstContainer();
+            }
+            else
+            {
+                Console.WriteLine("Choose the correct operation! >:| ");
+                operationAnswer();
+            }
+        }
+
+   
+    }
+
+
+
+    class SecondPart
+    {
+
+        public int secondNumber;
+        public void secondContainer()
+        {
+            Console.Write("Enter a second number: ");
+            try
+            {
+                secondNumber = Int32.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                secondContainer();
+            }
+        }
+
+        public void finalAnswer()
+        {
+            if ( FirstPart.questionAnswer == "+")
+            {
+                Console.WriteLine($"The answer is {FirstPart.firstNumber + secondNumber}");
+            }
+            else if (FirstPart.questionAnswer == "-")
+            {
+                Console.WriteLine($"The answer is {FirstPart.firstNumber - secondNumber}");
+            }
+            else if (FirstPart.questionAnswer == "*")
+            {
+                Console.WriteLine($"The answer is {FirstPart.firstNumber * secondNumber}");
+            }
+            else if (FirstPart.questionAnswer == "/")
+            {
+                Console.WriteLine($"The answer is {FirstPart.firstNumber / secondNumber}");
+            }
+         
+        }
+
+    }
+}
---
+++ FirstPoject/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using myFirstProject.Computation;
+
+
+
+
+namespace FirstPoject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an operation to use? + - * /");
+
+            FirstPart Calcuone = new FirstPart();
+
+            SecondPart Calcutwo = new SecondPart(); 
+
+            Calcuone.operationAnswer();
+
+            Calcutwo.secondContainer();
+
+            Calcutwo.finalAnswer();
+
+           
+    
+           
+   
+        }
+    }
+}
